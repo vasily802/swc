40a68a01e76083ee8a4a43fe1b1aa5d7dc54a54d
Vasily Volkov <vav802@gmail.com>
2019-01-06 22:12:28 -0800

    Monad

---
+++ src/main/scala/sandbox/Monad.scala
@@ -0,0 +1,12 @@
+package sandbox
+
+import scala.language.higherKinds
+
+trait Monad[F[_]] {
+  def pure[A](a: A): F[A]
+
+  def flatMap[A, B](value: F[A])(func: A => F[B]): F[B]
+
+  def map[A, B](value: F[A])(func: A => B): F[B] =
+    flatMap(value)(func andThen pure)
+}
